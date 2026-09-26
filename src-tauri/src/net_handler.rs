4ff514d09c2b0668c7f66e1eca4760362a8ab532
bethany <bth20xx@gmail.com>
2026-09-25 20:42:43 -0600

    Hotfix

--- src-tauri/src/net_handler.rs
+++ src-tauri/src/net_handler.rs
@@ -48,13 +48,11 @@ pub async fn main(app: AppHandle, mut cmd_rx: Receiver<String>) -> Result<(), Bo
             };
 
             let gossipsub_config = gossipsub::ConfigBuilder::default()
-                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
-                // signing)
-                .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .heartbeat_interval(Duration::from_secs(10))
+                .validation_mode(gossipsub::ValidationMode::Strict)
+                .message_id_fn(message_id_fn)
                 .build()
                 .map_err(io::Error::other)?;
-            // build a gossipsub network behaviour
             let gossipsub = gossipsub::Behaviour::new(
                 gossipsub::MessageAuthenticity::Signed(key.clone()),
                 gossipsub_config,
@@ -80,7 +78,7 @@ pub async fn main(app: AppHandle, mut cmd_rx: Receiver<String>) -> Result<(), Bo
                 if let Err(e) = swarm
                     .behaviour_mut().gossipsub
                     .publish(topic.clone(), line.as_bytes()) {
-                        if !matches!{e, gossipsub::PublishError::NoPeersSubscribedToTopic} {
+                        if !matches!{e, gossipsub::PublishError::NoPeersSubscribedToTopic} && !matches!{e, gossipsub::PublishError::Duplicate} {
                             println!("Publish error: {e:?}");
                         }
                 }
